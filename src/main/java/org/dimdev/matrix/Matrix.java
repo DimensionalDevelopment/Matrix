5435c759a56f24808cb5c5c7754d1fc90de9e6e7
SD <harrypotter.starwars.shrish@gmail.com>
2021-03-03 18:29:41 +0530

    Tame the beast (reflection)

--- src/main/java/org/dimdev/matrix/Matrix.java
+++ src/main/java/org/dimdev/matrix/Matrix.java
@@ -1,58 +1,52 @@
 package org.dimdev.matrix;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Optional;
 
-import java.util.ArrayList;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
+/**
+ * An Annotation Based Registration Library.
+ *
+ * <p>Matrix allows registering items, blocks, etc. without
+ * calling {@code Registry.register(...)} a bunch of times or
+ * registering at static init. Registering at static init
+ * is not safe as you might just register the entries much
+ * before vanilla registers its entries.</p>
+ *
+ * <p>Using Annotations is simple and easy to migrate to.
+ * You no longer have to worry about skipping a call to
+ * {@code Registry.register(...)}.</p>
+ */
 public class Matrix {
-
-    Logger LOG = LogManager.getLogger("Matrix");
-
-    private static ArrayList blockQueue;
-    private static ArrayList itemQueue;
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent e) {
-        LOG.info("Preinitialization Stage");
-    }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent e) {
-        LOG.info("Initialization Stage");
-    }
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent e) {
-        LOG.info("Post Initialization Stage");
-    }
-
-    public static void initializeBlockQueue() {
-        blockQueue = new ArrayList<>();
-    }
-
-    public static void initializeItemQueue() {
-        itemQueue = new ArrayList<>();
-    }
-
-    public static void addBlock(Object block) {
-        blockQueue.add(block);
-    }
-
-    public static void addItem(Object item) {
-        itemQueue.add(item);
-    }
-
-    public static ArrayList getBlockQueue() {
-        return blockQueue;
-    }
-
-    public static ArrayList getItemQueue() {
-        return itemQueue;
-    }
-
+	/**
+	 *
+	 * @param clazz The class that should be scanned for registry entries.
+	 * @param registry The registry that entries should be registered to.
+	 * @param entryClass The class that each registry entry should be an instance of.
+	 * @param <T> The registry's container type.
+	 */
+	public static <T> void register(Class<?> clazz, Registry<T> registry, Class<T> entryClass) {
+		String modId = Optional.ofNullable(clazz.getAnnotation(Register.class)).orElseThrow(() -> new UnsupportedOperationException(clazz.getName() + " did not have the Register annotation!")).modid();
+		Arrays.stream(clazz.getFields())
+				.filter(field -> {
+					int modifiers = field.getModifiers();
+					return Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers) && Modifier.isFinal(modifiers);
+				})
+				.filter(field -> field.isAnnotationPresent(RegistryEntry.class))
+				.filter(field -> entryClass.isAssignableFrom(field.getType()))
+				.forEach(field -> {
+					T value;
+					try {
+						//noinspection unchecked
+						value = (T) field.get(null);
+					} catch (IllegalAccessException e) {
+						throw new AssertionError(e); // cant happen
+					}
+					Identifier id = new Identifier(modId, field.getAnnotation(RegistryEntry.class).value());
+					Registry.register(registry, id, value);
+				});
+	}
 }
---
+++ src/main/java/org/dimdev/matrix/Register.java
@@ -0,0 +1,18 @@
+package org.dimdev.matrix;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Specifies the namespace for registry entries.
+ *
+ * <p>This must be annotated on the class you wish
+ * to keep all your registry entries (as fields).</p>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Register {
+	String modid();
+}
