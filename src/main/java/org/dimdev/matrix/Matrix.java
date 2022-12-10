1ee4d63262e109c9d15f77e6d1aed01e4d8a1ab6
Shrish Deshpande <shrish.deshpande@icloud.com>
2022-12-10 13:39:44 +0530

    1.19.3

--- src/main/java/org/dimdev/matrix/Matrix.java
+++ src/main/java/org/dimdev/matrix/Matrix.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registry;
 
 /**
  * An Annotation Based Registration Library.
--- src/testmod/java/org/dimdev/matrix/test/Test.java
+++ src/testmod/java/org/dimdev/matrix/test/Test.java
@@ -2,14 +2,14 @@ package org.dimdev.matrix.test;
 
 import org.dimdev.matrix.Matrix;
 
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registries;
 
 import net.fabricmc.api.ModInitializer;
 
 public class Test implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		Matrix.register(ModBlocks.class, Registry.BLOCK);
-		Matrix.register(ModItems.class, Registry.ITEM);
+		Matrix.register(ModBlocks.class, Registries.BLOCK);
+		Matrix.register(ModItems.class, Registries.ITEM);
 	}
 }
